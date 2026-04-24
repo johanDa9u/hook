f25d3710ee25b89331b70d46a72f5cedd8025947
Wassim Ayari <57444001+johanDa9u@users.noreply.github.com>
2026-04-24 23:23:14 +0200

    Update hook.js

--- scripts/hook.js
+++ scripts/hook.js
@@ -1,10 +1,8 @@
 Hooks.on("targetToken", async (user, token, targeted) => {
   const actor = token.actor;
   if (targeted) {
-    console.log(`${user.name} targeted ${token.name}`);
     await actor.toggleStatusEffect("aimed", { active: true, overlay: true });
   } else {
-    console.log(`${user.name} untargeted ${token.name}`);
     await actor.toggleStatusEffect("aimed", { active: false, overlay: true });
   }
 });
